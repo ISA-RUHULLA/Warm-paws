cb892a4d235fd0118c52df79fd96f03286129132
unknown <isaruhullah035@gmail.com>
2025-10-24 10:01:15 +0600

    Not found page recreate

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import NotFoundIcon from '../assets/error-404.png'
+import { useNavigate } from 'react-router-dom';
 
-const NotFound = () => {
+const NotFoundPage = () => {
+    const navigate = useNavigate();
     return (
         <div>
-            <h2>Sorry bro! Ei page khuje pelam na</h2>
+            <div className='flex flex-col justify-center items-center'>
+                <img src={NotFoundIcon} alt="OPPS!! APP NOT FOUND" className='w-74 h-74' />
+                <h1 className='font-bold text-3xl text-black mt-5'>OPPS!! APP NOT FOUND</h1>
+                <p className='font-normal text-black mt-2'>The App you are requesting is not found on our system.  please try another apps</p>
+                <div>
+                    <button onClick={() => navigate("/")} className='border rounded bg-purple-700 hover:bg-purple-500 text-white font-bold py-1 px-2 mt-3 mb-4 cursor-pointer'>Go Back</button>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default NotFound;
+export default NotFoundPage;
